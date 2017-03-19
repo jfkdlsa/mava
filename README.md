8c8ade7e02b184a27eeaa7c7d785a7bab6a94006
Ik <y8979326535@yahoo.com>
2017-03-19 14:28:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+mava
+====
+
+A Symfony project created on March 5, 2017, 3:48 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction( Request $request )
+    {
+        // replace this example code with whatever you need
+        return $this->render( 'default/index.html.twig', [
+            'base_dir' => realpath( $this->getParameter( 'kernel.root_dir' ) . '/..' ) . DIRECTORY_SEPARATOR,
+        ] );
+    }
+
+    /**
+     * @Route("/about/{name}", name="aboutpage", defaults={"name":null})
+     */
+    public function aboutAction( $name )
+    {
+        if ( $name )
+        {
+            $user = $this->getDoctrine()->getRepository( 'AppBundle:User' )->findOneBy( [ 'name' => $name ] );
+            if ( false === $user instanceof User )
+            {
+                throw $this->createNotFoundException( 'No user named ' . $name . ' found!' );
+            }
+        }
+        return $this->render( 'about/index.html.twig', [
+            'user' => $user
+        ] );
+    }
+}
